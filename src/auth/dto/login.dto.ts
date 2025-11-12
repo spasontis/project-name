98565f91aaa0f6a72d2a44b3c1bf13ebb978a9ab
Dmitrii <smxxtdzmail@gmail.com>
2025-11-12 19:17:56 +0700

    feat: add swagger (#6)
    **Description**
     - Added swagger api
     - Added new route 'api-docs'
     - Installed @nest/swagger and swagger-ui-express

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'login',
+    description: 'User login',
+  })
   @IsString({ message: 'Login must be a string' })
   @IsNotEmpty({ message: 'Login is required' })
   login: string;
 
+  @ApiProperty({
+    example: 'password',
+    description: 'User password',
+  })
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
